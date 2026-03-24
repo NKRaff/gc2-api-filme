6d59df2c545775284df06c0905eb7c8de75d2402
Rafael Rocha <rafaelgus3000@gmail.com>
2026-03-24 14:52:56 -0300

    feat: cria endpoint para cadastrar um filme

--- src/server.js
+++ src/server.js
@@ -1,10 +1,30 @@
 import express, { json } from "express"
 import { configDotenv } from "dotenv"
+import { filmes } from "./filmes.mock.js"
+import { v7 } from "uuid"
 
 const app = express()
 const porta = process.env.SERVER_PORTA || 3000
 
 app.use(express.json())
+
+app.post("/api/filmes", (req, res) => {
+  const { titulo, ano, generos } = req.body
+  if(titulo, ano, generos) {
+    const filme = {
+      id: v7(),
+      titulo,
+      ano,
+      generos
+    }
+
+    filmes.push(filme)
+
+    return res.status(201).json({message: "Filme cadastrado com sucesso"})
+  }
+  return res.status(400).json({error: "Campos obrigatórios: titulo, ano e generos"})
+})
+
 app.listen(porta, (error) => {
   if(error) {
     console.log(`🖥️  Erro ao iniciar servidor na porta ${porta}`)
