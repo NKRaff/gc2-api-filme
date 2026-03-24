7f4fbdb0e7377f63d3704e7de67d10dd4d316519
Rafael Rocha <rafaelgus3000@gmail.com>
2026-03-24 14:30:41 -0300

    feat: cria mock de dados de filmes

---
+++ src/filmes.mock.js
@@ -0,0 +1,34 @@
+import { v7 } from "uuid"
+
+export const filmes = [
+  {
+    id: v7(),
+    titulo: "A Volta dos que Não Foram",
+    ano: 1999,
+    generos: ["Comédia", "Aventura"]
+  },
+  {
+    id: v7(),
+    titulo: "As Tranças do Rei Careca",
+    ano: 2005,
+    generos: ["Comédia", "Fantasia"]
+  },
+  {
+    id: v7(),
+    titulo: "O Ataque dos Pães Voadores",
+    ano: 2012,
+    generos: ["Comédia", "Ficção Científica"]
+  },
+  {
+    id: v7(),
+    titulo: "Missão Quase Impossível: Esqueci de Tentar",
+    ano: 2020,
+    generos: ["Comédia", "Ação"]
+  },
+  {
+    id: v7(),
+    titulo: "De Repente, Nada Aconteceu",
+    ano: 2010,
+    generos: ["Comédia"]
+  }
+]
