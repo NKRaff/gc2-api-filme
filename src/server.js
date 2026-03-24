8807dfce0b587b44a16ddcec25d085aec33bbbb3
Rafael Rocha <rafaelgus3000@gmail.com>
2026-03-24 14:12:54 -0300

    feat: iniciar servidor express

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express, { json } from "express"
+import { configDotenv } from "dotenv"
+
+const app = express()
+const porta = process.env.SERVER_PORTA || 3000
+
+app.use(express.json())
+app.listen(porta, (error) => {
+  if(error) {
+    console.log(`🖥️  Erro ao iniciar servidor na porta ${porta}`)
+    console.error(`🖥️  Error: ${error.message}`)
+  } else {
+    console.log(`🖥️  Server rodando na porta ${porta}`)
+  }
+})
